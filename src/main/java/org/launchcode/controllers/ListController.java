9d948192bea685917c6fabd077bb02a57034d6f9
Sehar <sfsial@gmail.com>
2018-05-26 13:26:12 -0500

    Overrode search method in SearchController.java Added table and looped jobs in ListJobs.html to display all jobs Added table and looped jobs in search.html to display all jobs

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -33,7 +33,13 @@ public class ListController {
 
         return "list";
     }
-
+    /**In the listColumnValues method, the controller uses the query parameter
+     * passed in as column to determine which values to fetch from JobData.
+     * In the case of "all" it will fetch all job data, and then render
+     * list-jobs.html view template. In all other cases, it fetches only the values
+     * for the given column and passes them to the list-column.html view template.
+     * We'll explore these templates in a moment.
+     */
     @RequestMapping(value = "values")
     public String listColumnValues(Model model, @RequestParam String column) {
 
@@ -51,6 +57,18 @@ public class ListController {
         }
 
     }
+
+    /**In the listJobsByColumnAndValue method, we take in two query parameters:
+     * column and value. This has the net result of working similarly to the
+     * search functionality, in that we are "searching" for a particular value
+     * within a particular column and then displaying jobs that match. However,
+     * this is slightly different from the other way of searching in that the user
+     * will arrive at this handler method as a result of clicking on a link within
+     * one of our views, rather than via submitting a form. We'll see where these
+     * links originate when we look at the views. Also note that the
+     * listJobsByColumnAndValue method doesn't deal with an "all" scenario;
+     * it only displays jobs matching a specific value in a specific column.
+     */
 
     @RequestMapping(value = "jobs")
     public String listJobsByColumnAndValue(Model model,
--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,4 +24,16 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value="results")
+    public String search(Model model,String searchType, String searchTerm){
+        model.addAttribute("columns", ListController.columnChoices);
+        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("jobs", jobs);
+        System.out.println("column is "+ searchType);
+        System.out.println("searh Term "+searchTerm);
+        System.out.println("REsults size"+ jobs.size());
+        return "search";
+    }
+
 }
